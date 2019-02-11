4b6ed6e023113dab5d5b957fbe62fd72ca214f28
Alexander Ivanov <33380175+xanderhub@users.noreply.github.com>
2019-02-12 00:01:50 +0200

    bug fix + extend profile with user info

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -25,11 +25,13 @@ router.get(
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
           return res.status(400).json(errors);
         }
+        res.json(profile);
       })
       .catch(err => res.status(404).json(err));
   }
