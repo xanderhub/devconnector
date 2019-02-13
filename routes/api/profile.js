1d86d8e26608a48e01dccc4b2e77a5ae01ec9771
Alexander Ivanov <33380175+xanderhub@users.noreply.github.com>
2019-02-13 02:05:41 +0200

    add delete routes to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -226,4 +226,67 @@ router.post(
   }
 );
 
+//@route   DELETE api/profile/experience/:exp_id
+//@desc    Delete experience
+//@access  Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        //Filter exps
+        const newExpArray = profile.experience.filter(function(item) {
+          return item.id !== req.params.exp_id;
+        });
+
+        //Update exp
+        profile.experience = newExpArray;
+
+        //Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+//@route   DELETE api/profile/education/:edu_id
+//@desc    Delete education
+//@access  Private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        //Filter exps
+        const newEduArray = profile.education.filter(function(item) {
+          return item.id !== req.params.edu_id;
+        });
+
+        //Update exp
+        profile.education = newEduArray;
+
+        //Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+//@route   DELETE api/profile
+//@desc    Delete user & profile
+//@access  Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findByIdAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
+
 module.exports = router;
